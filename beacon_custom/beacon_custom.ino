f87f005b574662f5e02e68760fcbd4a4c8bfabb7
Martin Tan <MartinAnthonyBSutanto@gmail.com>
2016-12-20 10:09:45 +0100

    no message

--- beacon_custom/beacon_custom.ino
+++ beacon_custom/beacon_custom.ino
@@ -1,27 +1,28 @@
 // beacon for the ae3535 communications practical exercise
-
+ 
 // Sample RFM69 receiver/gateway sketch, with ACK and optional encryption
 // Passes through any wireless received messages to the serial port & responds to ACKs
 // It also looks for an onboard FLASH chip, if present
 // Library and code by Felix Rusu - felix@lowpowerlab.com
 // Get the RFM69 and SPIFlash library at: https://github.com/LowPowerLab/
-
+ 
 #include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
 #include <SPI.h>
 #include <EEPROM.h>     //Math library from arduino
 #include <SPIFlash.h> //get it here: https://www.github.com/lowpowerlab/spiflash
-
-
+ 
+ 
 #define NODEID        1    //unique for each node on same network
 #define NETWORKID     10  //the same on all nodes that talk to each other
-#define GATEWAYID     1    // Must be 1 or 2??
+#define GATEWAYID     2    // Must be 1 or 2??
 #define FREQUENCY     RF69_868MHZ
 #define ENCRYPTKEY    "ae3535-practicaz" //exactly the same 16 characters/bytes on all nodes!
 #define SERIAL_BAUD   115200
-
+#define MASTERBEAT    1000
+ 
 #define THRESHOLD_YAW 0.03
 #define THRESHOLD_ROLL 0.05
-
+ 
 #ifdef __AVR_ATmega1284P__
 #define LED           15 // Moteino MEGAs have LEDs on D15
 #define FLASH_SS      23 // and FLASH SS on D23
@@ -29,10 +30,10 @@
 #define LED           4 // Moteinos have LEDs on D9
 #define FLASH_SS      8 // and FLASH SS on D8
 #endif
-
+ 
 int led = 9; // Pin 9 has an LED connected in the moteino board
 int cc = 0;
-
+ 
 typedef struct {
   int           nodeId; //store this nodeId
   float         v1; // Voltages
@@ -40,36 +41,37 @@ typedef struct {
   float         v2b;
   float         v3;
 } Payload;
-
+ 
 Payload dataPackage;
-
+ 
 typedef struct {
   byte brightLeft; //Brightness levels for the LEDS
   byte brightRight;
   byte brightUp;
   byte brightDown;
 } Lightload;
-
+ 
 Lightload lightPackage;
 
+//Determination of Yaw Rotation (around z-axis; a.k.a.: "left/right rotation")
 byte brightnessL; //Brightness levels for the LEDS
 byte brightnessR;
 byte brightnessU;
 byte brightnessD;
 
-float v2a;
-float v2b;
 float diffLR ; //Declaring variables for voltage differences L-R and U-D
 float diffLR1;
 float diffUD;
 float diffUD1;
 int LR1;
 int UD1;
-
+ 
+ 
+ 
 RFM69 radio;
 SPIFlash flash(FLASH_SS, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
 bool promiscuousMode = false; //set to 'true' to sniff all packets on the same network
-
+ 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   Serial.print("This is the beacon ");
@@ -79,12 +81,9 @@ void setup() {
   radio.setHighPower(); //only for RFM69HW!
 #endif
   radio.encrypt(ENCRYPTKEY);
-  //radio.promiscuous(promiscuousMode);
-  //radio.setFrequency(919000000);
   char buff[50];
   sprintf(buff, "listening at %d MHz...", FREQUENCY == RF69_433MHZ ? 433 : FREQUENCY == RF69_868MHZ ? 868 : 915);
   Serial.println(buff);
-  //if (flash.initialize())
   if (flash.initialize()) {
     Serial.print("SPI Flash Init OK. Unique MAC = [");
     flash.readUniqueId();
@@ -94,92 +93,17 @@ void setup() {
       if (i != 8) Serial.print(':');
     }
     Serial.println(']');
-    //alternative way to read it:
-    //byte* MAC = flash.readUniqueId();
-    //for (byte i=0;i<8;i++)
-    //{
-    //  Serial.print(MAC[i], HEX);
-    //  Serial.print(' ');
-    //}
+    }
   }
-  else
-    Serial.println("SPI Flash Init FAIL! (is chip present?)");
-}
+  //else
+    //Serial.println("SPI Flash Init FAIL! (is chip present?)");
 
+ 
 byte ackCount = 0;
 uint32_t packetCount = 0;
 int echoon = 1;
-
+ 
 void loop() {
-  //process any serial input
-  if (Serial.available() > 0)
-  {
-    char input = Serial.read();
-    if (input == 'h') { // print some help
-      Serial.println("h for help");
-      Serial.println("r for dump all registers");
-      Serial.println("E to enable encryption");
-      Serial.println("e to disable encryption");
-      Serial.println("p for promiscuous mode");
-      Serial.println("d to dump the flash area");
-      Serial.println("D to delete the flash area");
-      Serial.println("i to show the device id");
-      Serial.println("t to display the radio temperature");
-      Serial.println("z toggle echo on/off");
-    }
-    if (input == 'r') //d=dump all register values
-      radio.readAllRegs();
-    if (input == 'E') { //E=enable encryption
-      radio.encrypt(ENCRYPTKEY);
-      Serial.print("Encryption enabled with key "); Serial.println(ENCRYPTKEY);
-    }
-    if (input == 'e') { //e=disable encryption
-      radio.encrypt(null);
-      Serial.println("Encryption disabled");
-    }
-    if (input == 'p') {
-      promiscuousMode = !promiscuousMode;
-      radio.promiscuous(promiscuousMode);
-      Serial.print("Promiscuous mode "); Serial.println(promiscuousMode ? "on" : "off");
-    }
-    if (input == 'd') //d=dump flash area
-    {
-      Serial.println("Flash content:");
-      int counter = 0;
-      while (counter <= 256) {
-        Serial.print(flash.readByte(counter++), HEX);
-        Serial.print('.');
-      }
-      while (flash.busy());
-      Serial.println();
-    }
-    if (input == 'D') {
-      Serial.print("Deleting Flash chip ... ");
-      flash.chipErase();
-      while (flash.busy());
-      Serial.println("DONE");
-    }
-    if (input == 'i') {
-      Serial.print("DeviceID: ");
-      word jedecid = flash.readDeviceId();
-      Serial.println(jedecid, HEX);
-    }
-    if (input == 't')
-    {
-      byte temperature =  radio.readTemperature(-1); // -1 = user cal factor, adjust for correct ambient
-      byte fTemp = 1.8 * temperature + 32; // 9/5=1.8
-      Serial.print( "Radio Temp is ");
-      Serial.print(temperature);
-      Serial.print("C, ");
-      Serial.print(fTemp); //converting to F loses some resolution, obvious when C is on edge between 2 values (ie 26C=78F, 27C=80F)
-      Serial.println('F');
-    }
-    if (input == 'z') {
-      echoon = 1 - echoon;
-      Serial.print("Echo ");
-      Serial.println(echoon);
-    }
-  }
   if (radio.receiveDone()) {
     if (echoon) {
       Serial.print("#[");
@@ -194,97 +118,49 @@ void loop() {
         Serial.print("] ");
       }
       Serial.print("Values received: \n");
-
+ 
       // Printing payload data.
       Payload* pay = (Payload*)radio.DATA;
+      
       Serial.print("<NodeID "); Serial.print(pay->nodeId); Serial.print(" >\n");
       Serial.print("<Voltage 1 "); Serial.print(pay->v1); Serial.print(" >\n");
       Serial.print("<Voltage 2 "); Serial.print(pay->v2a); Serial.print(" >\n");
       Serial.print("<Voltage 2b "); Serial.print(pay->v2b); Serial.print(" >\n");
       Serial.print("<Voltage 3 "); Serial.print(pay->v3); Serial.print(" >\n");
-
-    } else {
-      delay(10);
-    }
-
-    if (radio.ACKRequested()) {
-      byte theNodeID = radio.SENDERID;
-      radio.sendACK();
-      if (echoon) Serial.print(" - ACK sent.");
-      // When a node requests an ACK, respond to the ACK
-      // and also send a packet requesting an ACK (every 3rd one only)
-      // This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
-      if (ackCount++ % 3 == 0) {
-        if (echoon) {
-          Serial.print(" Pinging node ");
-          Serial.print(theNodeID);
-          Serial.print(" - ACK...");
-        } else {
-          delay(10);
-        }
-        delay(3); //need this when sending right after reception .. ?
-        if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0))  { // 0 = only 1 attempt, no retries
-          if (echoon) {
-            Serial.print("ok!");
-          } else {
-            delay(10);
-          }
-        } else {
-          if (echoon) {
-            Serial.print("nothing");
-          } else {
-            delay(10);
-          }
-        }
-      }
-    }
-    float v1;
-    float v2;
-    float v3;
-
-    //Determination of Yaw Rotation (around z-axis; a.k.a.: "left/right rotation")
+      
+      float v1 = pay->v1;
+      float v2a = pay->v2a;
+      float v2b = pay->v2b;
+      float v3 = pay->v3;
+    
     diffLR  = v1 - v3;
-    diffLR1 = constrain(diffLR, -THRESHOLD_YAW, THRESHOLD_YAW); //Constraining the value of diff LR1;
+    //diffLR1 = constrain(diffLR, -THRESHOLD_YAW, THRESHOLD_YAW); //Constraining the value of diff LR1;
 
-    if (diffLR1 >= 0) {
       // Maps the position of diffLR1 in the range of [0.0, 0.36] to an integer from [1, 6]
-      int LR1 = map(diffLR1, 0.0, 0.36, 1, 6);
-    } else {
-      // Maps the position of diffLR1 in the range of [-0.36, 0] to an integer from [-6, -1]
-      int LR1 = map(diffLR1, -0.36, 0.0, -6, -1);
-    }
-
-    if (diffLR1 != diffLR) {
-      if (LR1 > 0) {
+     int LR1 = map(abs(diffLR), 0.03, 0.4, 1, 6);
+ 
+     if (diffLR > 0) {
         if (LR1 == 6) {
           brightnessL = 255.0;
+          brightnessR = 0.0;
         } else {
           brightnessL = 0.0;
-        }
+        
         brightnessR = LR1 * 40.0;
-      } else if (LR1 < 0) {
+        }
+      } else if (diffLR < 0) {
         if (LR1 == -6) {
           brightnessR = 255.0;
         } else {
           brightnessR = 0.0;
         }
         brightnessL = abs(LR1) * 40.0;
-      } else {
-        Serial.print("Error");
       }
-    } else if (v2a > 0.6 || v2b > 0.6) {
-      brightnessR = 255.0;
-      brightnessL = 255.0;
-    } else {
-      brightnessR = 0.0;
-      brightnessL = 0.0;
-    }
-
     //Determination of Roll Rotation (around x-axis --> Up/Down Motion)
     diffUD = v2b - v2a;
     diffUD1 = constrain(diffUD, -THRESHOLD_ROLL, THRESHOLD_ROLL);
-    int UD1 = map(abs(diffUD), 0.0, 0.36, 1, 6);
-
+   int UD1 = map(abs(diffUD), 0.0, 2.0, 1, 6);
+ 
     if (diffUD1 == diffUD) {
       brightnessU = 255.0;
       brightnessD = 255.0;
@@ -305,29 +181,42 @@ void loop() {
         brightnessU = 0.0;
       }
     }
-
+ 
     lightPackage.brightLeft = brightnessL;
     lightPackage.brightRight = brightnessR;
     lightPackage.brightUp = brightnessU;
     lightPackage.brightDown = brightnessD;
+    
+    Serial.print(v1);Serial.print("\n");
+    Serial.print(v3);Serial.print("\n");
+    Serial.print(brightnessL);Serial.print("\n");
+    Serial.print(brightnessR);Serial.print("\n");
+    Serial.print("Constrained LR1 ");   Serial.print(diffLR1);Serial.print("\n");
+    Serial.print("diffLR v1-v3 ");Serial.print(diffLR);Serial.print("\n");
+    Serial.print("LR1 ");Serial.print(LR1);Serial.print("\n");
+    
+    } else {
+      delay(10);
+    }
+ 
+    if (radio.ACKRequested()) {
+      byte theNodeID = radio.SENDERID;
+      radio.sendACK();
+      if (echoon) Serial.println(" - ACK sent.");
+    }
 
-    if (radio.sendWithRetry(GATEWAYID, (const void*)(&lightPackage), sizeof(lightPackage))) {
+ 
+    if (radio.sendWithRetry(GATEWAYID, (const void*)(&lightPackage), sizeof(lightPackage), 12, 300)) {
       Serial.print("GS sent data back\n");
     } else {
-      Serial.print("nothing...\n");
+      Serial.print("nothing TEST...\n");
     }
   }
 }
-//if (echoon) Serial.println();
-//Blink(LED,3);
-//}
-
+ 
 void Blink(byte PIN, int DELAY_MS) {
   pinMode(PIN, OUTPUT);
   digitalWrite(PIN, HIGH);
   delay(DELAY_MS);
   digitalWrite(PIN, LOW);
 }
-
-
-
--- node_stc/node_stc.ino
+++ node_stc/node_stc.ino
@@ -9,8 +9,8 @@
 #define KEY         "ae3535-practicaz" //has to be same 16 characters/bytes on all nodes, not more not less!
 #define LED         9
 #define SERIAL_BAUD 115200
-#define MASTERBEAT    1000  // leave it like this please (preferable increase it)
-#define ACK_TIME    30  // # of ms to wait for an ack
+#define MASTERBEAT  2000  // leave it like this please (preferable increase it)
+#define ACK_TIME    500  // # of ms to wait for an ack
 
 #ifdef __AVR_ATmega1284P__
 #define LED           15 // Moteino MEGAs have LEDs on D15
@@ -20,7 +20,7 @@
 #define FLASH_SS      8 // and FLASH SS on D8
 #endif
 
-int TRANSMITPERIOD = 300; //transmit a packet to gateway so often (in ms)
+int TRANSMITPERIOD = 500; //transmit a packet to gateway so often (in ms)
 byte sendSize = 0;
 boolean requestACK = false;
 SPIFlash flash(FLASH_SS, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
@@ -29,8 +29,7 @@ RFM69 radio;
 typedef struct {
   int           nodeId; //store this nodeId
   float         v1; // Voltages
-  float         v2a;
-  float         v2b;
+  float         v2;
   float         v3;
 } Payload;
 Payload theData;
@@ -44,10 +43,29 @@ typedef struct {
 
 Lightload lightPackage;
 
-const int ledL = 3; // Assigning the LEDS to the digital ports connected to PWM
-const int ledR = 5;
-const int ledU = 6;
-const int ledD = 9;
+//Constants that won't change
+const int ledL = 6; // Assigning the LEDS to the digital ports connected to PWM
+const int ledR = 9;
+const int ledU = 5;
+const int ledD = 3;
+
+const int LDR1 = A1; // Assigning the sensors to the pins
+const int LDR2 = A0;
+const int LDR3 = A2;
+
+//Variables
+int v1 = 0;         // Sensor Value
+int v1Min = 1023;   // Minimum Sensor Value
+int v1Max = 0;      // Maximum Sensor Value
+
+int v2 = 0;        
+int v2Min = 1023;   
+int v2Max = 0;           
+
+int v3 = 0;         
+int v3Min = 1023;  
+int v3Max = 0;    
+
 
 void setup() {
   // Setting the pins in the correct mode
@@ -70,159 +88,132 @@ void setup() {
   sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY == RF69_433MHZ ? 433 : FREQUENCY == RF69_868MHZ ? 868 : 915);
   Serial.println(buff);
 
-  if (flash.initialize())
-    Serial.println("SPI Flash Init OK!");
-  else
-    Serial.println("SPI Flash Init FAIL! (is chip present?)");
+    // turn on LEDs to signal the start of the calibration period:
+  analogWrite(ledL, 255.0);
+  analogWrite(ledR, 255.0);
+  analogWrite(ledU, 255.0);
+  analogWrite(ledD, 255.0);
+
+  // calibrate during the first 10 seconds
+  while (millis() < 10000) {
+    v1 = analogRead(LDR1);
+    v2 = analogRead(LDR2);
+    v3 = analogRead(LDR3);
+
+    // record the maximum sensor value
+    if (v1 > v1Max) {
+      v1Max = v1;
+    }
+    
+    if (v2 > v2Max) {
+      v2Max = v2;
+    }
+
+    if (v3 > v3Max) {
+      v3Max = v3;
+    }
+
+    // record the minimum sensor value
+    if (v1 < v1Min) {
+      v1Min = v1;
+    }
+
+    if (v2 < v2Min) {
+      v2Min = v2;
+    }
+
+    if (v3 < v3Min) {
+      v3Min = v3;
+    }
+  }
+
+  // signal the end of the calibration period
+  analogWrite(ledL, 0.0);
+  analogWrite(ledR, 0.0);
+  analogWrite(ledU, 0.0);
+  analogWrite(ledD, 0.0);
 }
 
-long lastPeriod = -1;
 void loop() {
-  //process any serial input
+  long lastPeriod = -1;
+
   // -----------------------------------Determine voltages
-  float  LDR1;
-  float  LDR2a;
-  float  LDR2b;
-  float  LDR3;
-  float  Voltage1;
-  float  Voltage2a;
-  float  Voltage2b;
-  float  Voltage3;
+  // read the sensor:
+  v1 = analogRead(LDR1);
+  v2 = analogRead(LDR2);
+  v3 = analogRead(LDR3);
 
+  // apply the calibration to the sensor reading
 
-  LDR1 = analogRead(A0);
-  delay(10);
-  LDR1 = analogRead(A0);
-  delay(10);
-  Voltage1 = (LDR1 * 3.3) / 1024.0;
-  delay(10);
+  v1 = map(v1, v1Min, v1Max, 100, 255);
+  v2 = map(v2, v2Min, v2Max, 100, 255);
+  v3 = map(v3, v3Min, v3Max, 100, 255);
 
-  LDR2a = analogRead(A1);
-  delay(5000);
-  LDR2b = analogRead(A1);
-
-  Voltage2a = (LDR2a * 3.3) / 1024.0;
-  Voltage2b = (LDR2b * 3.3) / 1024.0;
-
-  LDR3 = analogRead(A2);
-  delay(10);
-  LDR3 = analogRead(A3);
-  delay(10);
-  Voltage3 = (LDR3 * 3.3) / 1024.0;
-  delay(10);
-
-  //-----------------------------fill in the struct with new values
-  theData.nodeId = NODEID;
-  theData.v1 = Voltage1; //
-  theData.v2a = Voltage2a;
-  theData.v2b = Voltage2b;
-  theData.v3 = Voltage3;
-
-  if (Serial.available() > 0)
-  {
-    char input = Serial.read();
-    if (input >= 48 && input <= 57) //[0,9]
-    {
-      TRANSMITPERIOD = 100 * (input - 48);
-      if (TRANSMITPERIOD == 0) TRANSMITPERIOD = 1000;
-      Serial.print("\nChanging delay to ");
-      Serial.print(TRANSMITPERIOD);
-      Serial.println("ms\n");
-    }
-
-    if (input == 'r') //d=dump register values
-      radio.readAllRegs();
-    //if (input == 'E') //E=enable encryption
-    //  radio.encrypt(KEY);
-    //if (input == 'e') //e=disable encryption
-    //  radio.encrypt(null);
-
-    if (input == 'd') //d=dump flash area
-    {
-      Serial.println("Flash content:");
-      int counter = 0;
-
-      while (counter <= 256) {
-        Serial.print(flash.readByte(counter++), HEX);
-        Serial.print('.');
-      }
-      while (flash.busy());
-      Serial.println();
-    }
-    if (input == 'e')
-    {
-      Serial.print("Erasing Flash chip ... ");
-      flash.chipErase();
-      while (flash.busy());
-      Serial.println("DONE");
-    }
-    if (input == 'i')
-    {
-      Serial.print("DeviceID: ");
-      word jedecid = flash.readDeviceId();
-      Serial.println(jedecid, HEX);
-    }
-  }
-
-  //check for any received packets
-  if (radio.receiveDone())
-  {
-    Serial.print('['); Serial.print(radio.SENDERID, DEC); Serial.print("] ");
-    for (byte i = 0; i < radio.DATALEN; i++)
-      Serial.print((char)radio.DATA[i]);
-    Serial.print("   [RX_RSSI:"); Serial.print(radio.readRSSI()); Serial.print("]");
-
-    if (radio.ACKRequested())
-    {
-      radio.sendACK();
-      Serial.print(" - ACK sent");
-      delay(10);
-    }
-
-    Serial.println();
-  }
-
-  int currPeriod = millis() / TRANSMITPERIOD;
-  if (currPeriod != lastPeriod)
-  {
-
-    Serial.print("Sending struct (");
-    Serial.print(sizeof(theData));
-    Serial.print(" bytes) ... ");
-    if (radio.sendWithRetry(GATEWAYID, (const void*)(&theData), sizeof(theData))) {
-      Serial.print(" ok!");
-      Serial.print(Voltage1);
-      Serial.print(Voltage2a);
-      Serial.print(Voltage2b);
-      Serial.print(Voltage3);
-    }
-    else Serial.print(" nothing...");
-    Serial.println();
-
-    byte brightnessL;
-    byte brightnessR;
-    byte brightnessU;
-    byte brightnessD;
-
-    // check if data has been sent from the computer:
-          // Printing payload data.
-      Lightload* light = (Lightload*)radio.DATA;
-      Serial.read();
-      // read the most recent byte (which will be from 0 to 255):
-      brightnessL = light->brightLeft;
-      brightnessR = light->brightRight;
-      brightnessU = light->brightUp;
-      brightnessD = light->brightDown;
-      // set the brightness of the LED:
-      analogWrite(ledL, brightnessL);
-      analogWrite(ledR, brightnessR);
-      analogWrite(ledU, brightnessU);
-      analogWrite(ledD, brightnessD);
-    }
-
-
-    lastPeriod = currPeriod;
+  // in case the sensor value is outside the range seen during calibration
+  v1 = constrain(v1, 100, 255);
+  v2 = constrain(v2, 100, 255);
+  v3 = constrain(v3, 100, 255);
   
+  // fade the LEDs using the calibrated value:
+    if (v1 > v3 && v2 >= v1 && v2 >= v3) {
+      analogWrite(ledL, v1);
+      analogWrite(ledR, 0.0);
+      analogWrite(ledU, v2);
+      analogWrite(ledD, 0.0);
+  }
+  
+    if (v3 > v1 && v2 > v1 && v2 >= v3) {
+      analogWrite(ledL, 0.0);
+      analogWrite(ledR, v3);
+      analogWrite(ledU, v2);
+      analogWrite(ledD, 0.0);
+    }
+
+    if (v3 > v1 && v2 > v1 && v2 < v3) {
+      analogWrite(ledL, 0.0);
+      analogWrite(ledR, v3);
+      analogWrite(ledU, 0.0);
+      analogWrite(ledD, 0.0);
+    }
+
+    if (v3 > v1 && v2 <= v1 && v2 < v3) {
+      analogWrite(ledL, 0.0);
+      analogWrite(ledR, v3);
+      analogWrite(ledU, 0.0);
+      analogWrite(ledD, v2);
+    }
+
+    if (v1 > v3 && v2 < v1 && v2 <= v3) {
+      analogWrite(ledL, v1);
+      analogWrite(ledR, 0.0);
+      analogWrite(ledU, 0.0);
+      analogWrite(ledD, v2);
+  }
+
+    if (v1 > v3 && v2 < v1 && v2 > v3) {
+      analogWrite(ledL, v1);
+      analogWrite(ledR, 0.0);
+      analogWrite(ledU, 0.0);
+      analogWrite(ledD, 0.0);
+  }
+
+    if (abs(v1-v3) <= 0.05 && v2 <= v1 && v2 <= v3) {
+      analogWrite(ledL, 0.0);
+      analogWrite(ledR, 0.0);
+      analogWrite(ledU, v2);
+      analogWrite(ledD, 0.0);
+  }
+
+  
+    if (abs(v1-v3) <= 0.05 && v2 >= v1 && v2 >= v3) {
+      analogWrite(ledL, 0.0);
+      analogWrite(ledR, v3);
+      analogWrite(ledU, 0.0);
+      analogWrite(ledD, 0.0);
+  }
+  Serial.print("Brightv1 = "); Serial.print(v1); Serial.print("\n");
+  Serial.print("Brightv2 = "); Serial.print(v2); Serial.print("\n");
+  Serial.print("Brightv3 = "); Serial.print(v3); Serial.print("\n");
 
 }
 
